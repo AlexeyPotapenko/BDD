9f61ba8066c67a3dcbd2c8354495cdf331d6e794
AlexeyPotapenko <alesha_potapenko@mail.ru>
2021-11-21 18:30:02 +0300

    add test

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,42 @@
+package ru.netology.web.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardNumInfo {
+        String cardNumber;
+
+    }
+
+    public static CardNumInfo getFirstCardNum() {
+        return new CardNumInfo("5559000000000001");
+    }
+
+    public static CardNumInfo getSecondCardNub() {
+        return new CardNumInfo("5559000000000002");
+    }
+
+}
---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,45 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+  private SelenideElement heading = $("[data-test-id=dashboard]");
+
+  public DashboardPage() {
+    heading.shouldBe(visible);
+  }
+
+  private ElementsCollection cards = $$(".list__item");
+  private final String balanceStart = "баланс: ";
+  private final String balanceFinish = " р.";
+
+  public int getOneCardBalance() {
+    val text = cards.first().text();
+    return extractBalance(text);
+  }
+
+  public int getTwoCardBalance() {
+    val text = cards.last().text();
+    return extractBalance(text);
+  }
+
+  public TransferPage selectCardToReplenishment(DataHelper.CardNumInfo cardInfo) {
+    cards.findBy(text(cardInfo.getCardNumber().substring(12, 16))).$("[data-test-id='action-deposit']").click();
+    return new TransferPage();
+  }
+
+  private int extractBalance(String text) {
+    val start = text.indexOf(balanceStart);
+    val finish = text.indexOf(balanceFinish);
+    val value = text.substring(start + balanceStart.length(), finish);
+    return Integer.parseInt(value);
+  }
+}
